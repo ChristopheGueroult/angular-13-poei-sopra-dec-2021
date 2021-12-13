f08e60a2f8af30e80aefb94c013f3f77be860d08
christophe <christophegueroult@gmail.com>
2021-12-13 11:27:48 +0100

    directive structural ngFor

--- src/app/shared/components/tab-light/tab-light.component.ts
+++ src/app/shared/components/tab-light/tab-light.component.ts
@@ -3,13 +3,37 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-tab-light',
   templateUrl: './tab-light.component.html',
-  styleUrls: ['./tab-light.component.scss']
+  styleUrls: ['./tab-light.component.scss'],
 })
 export class TabLightComponent implements OnInit {
-
-  constructor() { }
+  public headers!: string[];
+  public collection!: any[];
+  constructor() {}
 
   ngOnInit(): void {
+    this.headers = [
+      'Client',
+      'Type',
+      'Description',
+      'Unit Price',
+      'Nb Days',
+      'Tva',
+      'Total (excl. Taxes)',
+      'Total (with Taxes)',
+      'State',
+    ];
+    this.collection = [
+      {
+        id: 1,
+        label: 'Formation Java',
+        adrEt: 450,
+        numberOfDays: 5,
+        tva: 20,
+        status: 'CONFIRMED',
+        type: 'Forfait',
+        notes: 'Test',
+        customerId: 1,
+      },
+    ];
   }
-
 }
